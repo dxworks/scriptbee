213a123cad45065cc28967d9a4caf1dc57efd367
Tania Topciov <taniatopciov@yahoo.com>
2021-08-12 18:22:46 +0300

    Added JavascriptValidScriptExtractors and Delimiters

---
+++ ScriptBee/Utils/ValidScriptDelimiters.cs
@@ -0,0 +1,10 @@
+﻿namespace ScriptBee.Utils
+{
+    public static class ValidScriptDelimiters
+    {
+        public const string PythonStartComment = "# start script";
+        public const string PythonEndComment = "# end script";
+        public const string JavascriptStartComment = "// start script";
+        public const string JavascriptEndComment = "// end script";
+    }
+}
---
+++ ScriptBee/Utils/ValidScriptExtractors/JavascriptValidScriptExtractor.cs
@@ -0,0 +1,9 @@
+﻿namespace ScriptBee.Utils.ValidScriptExtractors
+{
+    public class JavascriptValidScriptExtractor : ValidScriptExtractor
+    {
+        protected override string StartComment => ValidScriptDelimiters.JavascriptStartComment;
+
+        protected override string EndComment => ValidScriptDelimiters.JavascriptEndComment;
+    }
+}
