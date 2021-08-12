6dad6ebcc702a9cf739672a001665a5a1a6b1606
Tania Topciov <taniatopciov@yahoo.com>
2021-08-12 18:25:07 +0300

    Added Tests for ScriptExtractors and TemplateGenerators

---
+++ ScriptBeeTests/Scripts/TemplateGenerators/TemplateGeneratorTest.cs
@@ -0,0 +1,56 @@
+﻿using ScriptBee.Models.Dummy;
+using ScriptBee.Scripts.TemplateGenerators;
+using ScriptBee.Scripts.TemplateGenerators.Strategies;
+using Xunit;
+
+namespace ScriptBeeTests.Scripts.TemplateGenerators
+{
+    public class TemplateGeneratorTest
+    {
+        [Fact]
+        public void Generate_WithPythonStrategy()
+        {
+            const string expectedScript = @"class DummyModel:
+    DummyNumber: int
+    DummyString: str
+    IsDummy: bool
+
+model: DummyModel
+
+# start script
+
+print(model)
+
+# end script
+";
+            string generatedScript =
+                new TemplateGenerator(new PythonStrategyTemplateGenerator()).Generate(typeof(DummyModel));
+
+            Assert.Equal(expectedScript, generatedScript);
+        }
+
+        [Fact]
+        public void Generate_WithJavascriptStrategy()
+        {
+            const string expectedScript = @"class DummyModel
+{
+    DummyNumber = 0;
+    DummyString = '';
+    IsDummy = true;
+}
+
+let model = new DummyModel();
+
+// start script
+
+print(model);
+
+// end script
+";
+            string generatedScript =
+                new TemplateGenerator(new JavascriptStrategyTemplateGenerator()).Generate(typeof(DummyModel));
+
+            Assert.Equal(expectedScript, generatedScript);
+        }
+    }
+}
---
+++ ScriptBeeTests/Utils/ValidScriptExtractors/JavascriptValidScriptExtractorTest.cs
@@ -0,0 +1,140 @@
+﻿using ScriptBee.Utils.ValidScriptExtractors;
+using Xunit;
+
+namespace ScriptBeeTests.Utils.ValidScriptExtractors
+{
+    public class JavascriptValidScriptExtractorTest
+    {
+        private readonly JavascriptValidScriptExtractor _javascriptExtractor;
+
+        public JavascriptValidScriptExtractorTest()
+        {
+            _javascriptExtractor = new JavascriptValidScriptExtractor();
+        }
+
+        [Fact]
+        public void ExtractValidScript_WithTextBefore()
+        {
+            const string script = @"
+class DummyModel {
+    DummyNumber = 0;
+    DummyString = '';
+    IsDummy = true;
+}
+
+let model = new DummyModel();
+
+// start script
+
+print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);
+
+// end script 
+";
+            const string validScript = @"print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);";
+
+            var extractedScript = _javascriptExtractor.ExtractValidScript(script);
+            Assert.Equal(validScript, extractedScript);
+        }
+
+        [Fact]
+        public void ExtractValidScript_WithTextAfter()
+        {
+            const string script = @"// start script
+
+print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);
+
+// end script 
+
+class DummyModel {
+    DummyNumber = 0;
+    DummyString = '';
+    IsDummy = true;
+}
+
+let model = new DummyModel();
+";
+            const string validScript = @"print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);";
+            string extractedScript = _javascriptExtractor.ExtractValidScript(script);
+            Assert.Equal(validScript, extractedScript);
+        }
+
+        [Fact]
+        public void ExtractValidScript_NoExtraText()
+        {
+            const string script = @"
+// start script
+
+print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);
+
+// end script
+";
+            const string validScript = @"print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);";
+            string extractedScript = _javascriptExtractor.ExtractValidScript(script);
+            Assert.Equal(validScript, extractedScript);
+        }
+
+        [Fact]
+        public void ExtractValidScript_TextBeforeAndAfter()
+        {
+            const string script = @"class DummyModel {
+    DummyNumber = 0;
+    DummyString = '';
+    IsDummy = true;
+}
+
+let model = new DummyModel();// start script
+
+print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);
+
+// end script 
+
+class DummyModel {
+    DummyNumber = 0;
+    DummyString = '';
+    IsDummy = true;
+}
+";
+            const string validScript = @"print(model.DummyNumber);
+print(model.DummyString);
+print(model.IsDummy);";
+
+            string extractedScript = _javascriptExtractor.ExtractValidScript(script);
+            Assert.Equal(validScript, extractedScript);
+        }
+
+        [Theory]
+        [InlineData(" ")]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("class DummyModel {")]
+        [InlineData(@"class DummyModel {
+
+// start script
+
+some text }")]
+        [InlineData(@"class DummyModel {
+
+// end script")]
+        public void ExtractValidScript_InvalidCases(string script)
+        {
+            const string invalidScript = "";
+
+            string extractedScript = _javascriptExtractor.ExtractValidScript(script);
+            Assert.Equal(invalidScript, extractedScript);
+        }
+    }
+}
