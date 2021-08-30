096d8e515225c3e14ae31465090940020e3dfc2a
Tania Topciov <taniatopciov@yahoo.com>
2021-08-30 18:23:33 +0300

    Added ScriptRunnerController to ScriptBeeWebApp

---
+++ ScriptBeeWebApp/Controllers/ScriptRunnerController.cs
@@ -0,0 +1,59 @@
+﻿using HelperFunctions;
+using Microsoft.AspNetCore.Mvc;
+using ScriptBee.Models.Dummy;
+using ScriptBee.Plugins;
+using ScriptBee.Scripts.ScriptRunners;
+
+namespace ScriptBeeWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScriptRunnerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post(ScriptRunnerArguments scriptRunnerArguments)
+        {
+            HelperFunctionsMapper helperFunctionsMapper = new HelperFunctionsMapper();
+
+            switch (scriptRunnerArguments.ModelType)
+            {
+                case "dummy":
+                {
+                    DummyModelLoader dummyModelLoader = new DummyModelLoader();
+                    var loadedModel = dummyModelLoader.LoadModel(scriptRunnerArguments.ModelJsonContent);
+                    
+                    switch (scriptRunnerArguments.ScriptType)
+                    {
+                        case "python":
+                        {
+                            var pythonDummyScriptRunner = new PythonDummyScriptRunner(helperFunctionsMapper);
+                            pythonDummyScriptRunner.RunScript(loadedModel,scriptRunnerArguments.ScriptContent);
+                            return Ok(scriptRunnerArguments);
+                        }
+                        case "javascript":
+                        {
+                            var javascriptDummyScriptRunner = new JavascriptDummyScriptRunner(helperFunctionsMapper);
+                            javascriptDummyScriptRunner.RunScript(loadedModel,scriptRunnerArguments.ScriptContent);
+                            return Ok(scriptRunnerArguments);
+                        }
+                        case "csharp":
+                        {
+                            // var cSharpDummyScriptRunner = new CSharpDummyScriptRunner(new PluginLoader(""));
+                            // cSharpDummyScriptRunner.RunScript(loadedModel,scriptRunnerArguments.ScriptContent);
+                            // return Ok(scriptRunnerArguments);
+                            return BadRequest("Not implemented");
+                        }
+                        default:
+                        {
+                            return BadRequest($"Script type {scriptRunnerArguments.ScriptType} is not supported");
+                        }
+                    }
+                }
+                default:
+                {
+                    return BadRequest($"Model type {scriptRunnerArguments.ModelType} is not supported");
+                }
+            }
+        }
+    }
+}
---
+++ ScriptBeeWebApp/ScriptRunnerArguments.cs
@@ -0,0 +1,15 @@
+﻿using ScriptBee.Models.Dummy;
+
+namespace ScriptBeeWebApp
+{
+    public class ScriptRunnerArguments
+    {
+        public string ModelType { get; set; }
+
+        public string ModelJsonContent { get; set; }
+
+        public string ScriptType { get; set; }
+
+        public string ScriptContent { get; set; }
+    }
+}
