98b744b1b20963292e34dc51751f528090a5ef69
Tania Topciov <taniatopciov@yahoo.com>
2021-09-03 16:06:17 +0300

    Added Endpoint for Loading Models When the Path is Given

--- ScriptBeeWebApp/Controllers/UploadModelController.cs
+++ ScriptBeeWebApp/Controllers/UploadModelController.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,7 +24,8 @@ namespace ScriptBeeWebApp.Controllers
 
         private readonly IProjectManager _projectManager;
 
-        public UploadModelController(IFolderWriter folderWriter, ILoadersHolder loadersHolder, IFileContentProvider fileContentProvider, IProjectManager projectManager)
+        public UploadModelController(IFolderWriter folderWriter, ILoadersHolder loadersHolder,
+            IFileContentProvider fileContentProvider, IProjectManager projectManager)
         {
             _folderWriter = folderWriter;
             _loadersHolder = loadersHolder;
@@ -32,9 +33,9 @@ namespace ScriptBeeWebApp.Controllers
             _projectManager = projectManager;
             _folderWriter.Initialize();
         }
-        
+
         [HttpPost("fromfile")]
-        public async Task<IActionResult> Post(IFormCollection formData)
+        public async Task<IActionResult> PostUsingFile(IFormCollection formData)
         {
             if (!formData.TryGetValue("modelType", out var modelType))
             {
@@ -49,7 +50,7 @@ namespace ScriptBeeWebApp.Controllers
             }
 
             List<string> fileContents = new List<string>();
-            
+
             foreach (var file in formData.Files)
             {
                 if (file.Length > 0)
@@ -62,9 +63,60 @@ namespace ScriptBeeWebApp.Controllers
             }
 
             var dictionary = modelLoader.LoadModel(fileContents);
-            
+
             _projectManager.AddToProject(dictionary, modelLoader.GetName());
-            
+
+            return Ok();
+        }
+
+        [HttpPost("frompath")]
+        public async Task<IActionResult> PostUsingPath(ScriptLoaderArguments scriptLoaderArguments)
+        {
+            if (scriptLoaderArguments.modelType == null)
+            {
+                return BadRequest("Missing model type");
+            }
+
+            var modelLoader = _loadersHolder.GetModelLoader(scriptLoaderArguments.modelType);
+
+            if (modelLoader == null)
+            {
+                return BadRequest($"Model type {scriptLoaderArguments.modelType} is not supported");
+            }
+
+            List<string> fileContents = new List<string>();
+
+            List<string> wrongPaths = new List<string>();
+
+            foreach (var modelPath in scriptLoaderArguments.paths)
+            {
+                var filePath = Path.Combine(ConfigFolders.PathToModels, modelPath);
+                try
+                {
+                    string fileContent = await _fileContentProvider.GetFileContentAsync(filePath);
+                    fileContents.Add(fileContent);
+                }
+                catch (Exception)
+                {
+                    wrongPaths.Add(modelPath);
+                }
+            }
+
+            if (wrongPaths.Count != 0)
+            {
+                string badRequestMessage = "Could not load models from the following paths:\n";
+                foreach (var wrongPath in wrongPaths)
+                {
+                    badRequestMessage = badRequestMessage + wrongPath + "\n";
+                }
+
+                return BadRequest(badRequestMessage);
+            }
+
+            var dictionary = modelLoader.LoadModel(fileContents);
+
+            _projectManager.AddToProject(dictionary, modelLoader.GetName());
+
             return Ok();
         }
     }
---
+++ ScriptBeeWebApp/ScriptLoaderArguments.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ScriptBeeWebApp
+{
+    public class ScriptLoaderArguments
+    {
+        public List<string> paths { get; set; }
+        public string modelType { get; set; }
+    }
+}
