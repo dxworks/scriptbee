e8d1b054567d414acf2c931a1535394fd0e459d2
Andrei Timar <atimar999@gmail.com>
2022-12-03 15:36:19 +0200

    Added ScriptBeeMarketplaceClientExtensions

--- ScriptBee.Marketplace.Client/ScriptBeeMarketplaceClientConfig.cs
+++ ScriptBee.Marketplace.Client/ScriptBeeMarketplaceClientConfig.cs
@@ -3,4 +3,6 @@
 public class ScriptBeeMarketplaceClientConfig
 {
     public string FirebaseConfigFile { get; init; } = null!;
+    public string FirestoreDbName { get; init; } = null!;
+    public string FirebaseStorageBucket { get; init; } = null!;
 }
---
+++ ScriptBee.Marketplace.Client/ScriptBeeMarketplaceClientExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Firebase.Storage;
+using Google.Cloud.Firestore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ScriptBee.Marketplace.Client.Data;
+using ScriptBee.Marketplace.Client.Repository;
+using ScriptBee.Marketplace.Client.Services;
+
+namespace ScriptBee.Marketplace.Client;
+
+public static class ScriptBeeMarketplaceClientExtensions
+{
+    public static void AddScriptBeeMarketplaceClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        var configurationSection = configuration.GetSection("ScriptBeeMarketplaceClient")
+            .Get<ScriptBeeMarketplaceClientConfig>();
+
+        if (configurationSection is null)
+        {
+            throw new InvalidFirebaseConfigException();
+        }
+
+        Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", configurationSection.FirebaseConfigFile);
+
+        services.AddSingleton<IDocumentRepository<Plugin>, FirebaseDocumentRepository<Plugin>>(_ =>
+            new FirebaseDocumentRepository<Plugin>(FirestoreDb.Create(configurationSection.FirestoreDbName)));
+        services.AddSingleton<IStorageRepository, FirebaseStorageRepository>(_ =>
+            new FirebaseStorageRepository(new FirebaseStorage(configurationSection.FirebaseStorageBucket)));
+        services.AddSingleton<IPluginFetcher, PluginFetcher>();
+    }
+}
