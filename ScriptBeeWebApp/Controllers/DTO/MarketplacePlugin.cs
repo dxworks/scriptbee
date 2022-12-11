a907c05ef015589622e264ddccb9924d66506001
Andrei Timar <atimar999@gmail.com>
2022-12-11 16:29:15 +0200

    Added Yaml Manifests

--- ScriptBeeWebApp/Controllers/DTO/MarketplacePlugin.cs
+++ ScriptBeeWebApp/Controllers/DTO/MarketplacePlugin.cs
@@ -13,16 +13,3 @@ public sealed record MarketplacePlugin(
 public sealed record PluginVersion(string Version, List<ExtensionPointVersion> ExtensionPointVersions, bool Installed);
 
 public sealed record ExtensionPointVersion(string Kind, string Version);
-
-// public sealed record MarketplaceBundlePlugin(
-//     string Id,
-//     string Name,
-//     string Description,
-//     string Author,
-//     string DownloadUrl,
-//     Dictionary<string, BundlePluginVersion> Versions
-// ) : BaseMarketplacePlugin(Id, Name, Description, Author, DownloadUrl, BundleType);
-//
-// public sealed record BundlePlugin(string Name, string Version, List<string> Kinds);
-//
-// public sealed record BundlePluginVersion(List<BundlePlugin> Plugins, bool Installed);
