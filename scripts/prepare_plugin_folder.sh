336a7195b6936a289ca013a205655018cd5a1acd
Andrei Timar <atimar999@gmail.com>
2022-12-24 09:20:37 +0200

    Updated plugin bundle scripts

--- scripts/prepare_plugin_folder.sh
+++ scripts/prepare_plugin_folder.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <version> <cs_proj_path> <output_path>"
-    exit 1
+if [ $# -lt 3 ]; then
+  echo "Usage: $0 <version> <cs_proj_path> <output_path> [other_folders...]"
+  exit 1
 fi
 
 version=$1
@@ -11,15 +11,23 @@ output_path=$3
 bin_path="$output_path/bin"
 
 if [ ! -f "$cs_proj_path" ]; then
-    echo "File $cs_proj_path does not exist"
-    exit 1
+  echo "File $cs_proj_path does not exist"
+  exit 1
 fi
 
 mkdir -p "$bin_path"
 
+echo "Copying other folders to $output_path"
+
+shift 3
+while [ $# -gt 0 ]; do
+  cp -r "$1" "$output_path"
+  shift
+done
+
 echo "Publishing DLLs to $bin_path"
 
-dotnet publish -o "$bin_path" -c Release -p:Version="$version" "$cs_proj_path" 
+dotnet publish -o "$bin_path" -c Release -p:Version="$version" "$cs_proj_path"
 
 echo "Copying DLLs to $output_path"
 
