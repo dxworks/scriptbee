bc843dc8b64eb305f2ffb0ab08255e472d753a2c
Andrei Timar <atimar999@gmail.com>
2025-02-24 20:31:40 +0200

    fix: update imports for Common.Web extensions

--- src/Adapters/Driving/Common.Web/Extensions/ProblemDetailsExtensions.cs
+++ src/Adapters/Driving/Common.Web/Extensions/ProblemDetailsExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
