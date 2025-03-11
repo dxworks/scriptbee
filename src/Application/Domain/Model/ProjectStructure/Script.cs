699f512c2a90f595b76fc12f4f630a61c37d5e00
Andrei Timar <atimar999@gmail.com>
2025-03-11 20:30:38 +0200

    feat: implement ICreateScript port

---
+++ src/Application/Domain/Model/ProjectStructure/Script.cs
@@ -0,0 +1,14 @@
+﻿using DxWorks.ScriptBee.Plugin.Api.Model;
+using ScriptBee.Domain.Model.Project;
+
+namespace ScriptBee.Domain.Model.ProjectStructure;
+
+public record Script(
+    ScriptId Id,
+    ProjectId ProjectId,
+    string Name,
+    string FilePath,
+    string AbsoluteFilePath,
+    string ScriptLanguage,
+    IEnumerable<ScriptParameter> Parameters
+);
