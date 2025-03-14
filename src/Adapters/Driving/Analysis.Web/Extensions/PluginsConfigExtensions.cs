fa89ea1174e9d3e1506cf55932262eb89a368eb9
Andrei Timar <atimar999@gmail.com>
2025-03-14 20:42:57 +0200

    feat: configure plugins for analysis service

---
+++ src/Adapters/Driving/Analysis.Web/Extensions/PluginsConfigExtensions.cs
@@ -0,0 +1,12 @@
+﻿using ScriptBee.Persistence.InMemory;
+using ScriptBee.Ports.Plugins;
+
+namespace ScriptBee.Analysis.Web.Extensions;
+
+public static class PluginsConfigExtensions
+{
+    public static IServiceCollection AddPluginsConfig(this IServiceCollection services)
+    {
+        return services.AddSingleton<IPluginRepository, PluginRepository>();
+    }
+}
