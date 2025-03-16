ac1179f82d20f54f323723657f54a5e1cf8996a7
Andrei Timar <atimar999@gmail.com>
2025-03-16 12:09:02 +0200

    feat: implement context clear endpoint

---
+++ src/Application/Domain/Service.Project/Context/ClearInstanceContextService.cs
@@ -0,0 +1,17 @@
+﻿using OneOf;
+using ScriptBee.Domain.Model;
+using ScriptBee.Domain.Model.Instance;
+using ScriptBee.UseCases.Project.Context;
+
+namespace ScriptBee.Service.Project.Context;
+
+public class ClearInstanceContextService : IClearInstanceContextUseCase
+{
+    public Task<OneOf<Unit, InstanceDoesNotExistsError>> Clear(
+        ClearContextCommand command,
+        CancellationToken cancellationToken = default
+    )
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Application/Ports/Driving/UseCases.Project/Context/IClearInstanceContextUseCase.cs
@@ -0,0 +1,13 @@
+﻿using OneOf;
+using ScriptBee.Domain.Model;
+using ScriptBee.Domain.Model.Instance;
+
+namespace ScriptBee.UseCases.Project.Context;
+
+public interface IClearInstanceContextUseCase
+{
+    Task<OneOf<Unit, InstanceDoesNotExistsError>> Clear(
+        ClearContextCommand command,
+        CancellationToken cancellationToken = default
+    );
+}
