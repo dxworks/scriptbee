2506fd87aa0d81113a1f51cccbc3b07d1b4c2d70
Andrei Timar <atimar999@gmail.com>
2025-03-23 18:10:27 +0200

    feat: implement LoadInstanceContextAdapter

---
+++ src/Adapters/Driven/Rest/Contracts/RestContextLoad.cs
@@ -0,0 +1,6 @@
+﻿namespace ScriptBee.Rest.Contracts;
+
+public class RestContextLoad
+{
+    public required IDictionary<string, List<string>> FilesToLoad { get; set; }
+}
--- src/Adapters/Driven/Rest/LoadInstanceContextAdapter.cs
+++ src/Adapters/Driven/Rest/LoadInstanceContextAdapter.cs
@@ -1,17 +1,38 @@
+using Refit;
 using ScriptBee.Domain.Model.File;
 using ScriptBee.Domain.Model.Instance;
 using ScriptBee.Ports.Instance;
+using ScriptBee.Rest.Api;
+using ScriptBee.Rest.Contracts;
 
 namespace ScriptBee.Rest;
 
 public class LoadInstanceContextAdapter(IHttpClientFactory httpClientFactory) : ILoadInstanceContext
 {
-    public Task Load(
+    public async Task Load(
         InstanceInfo instanceInfo,
         IDictionary<string, IEnumerable<FileId>> filesToLoad,
         CancellationToken cancellationToken = default
     )
     {
-        throw new NotImplementedException();
+        var client = httpClientFactory.CreateClient();
+        client.BaseAddress = new Uri(instanceInfo.Url);
+
+        var contextApi = RestService.For<IContextApi>(client);
+
+        await contextApi.Load(
+            new RestContextLoad { FilesToLoad = ConvertFilesToLoad(filesToLoad) },
+            cancellationToken
+        );
+    }
+
+    private static Dictionary<string, List<string>> ConvertFilesToLoad(
+        IDictionary<string, IEnumerable<FileId>> filesToLoad
+    )
+    {
+        return filesToLoad.ToDictionary(
+            x => x.Key,
+            y => y.Value.Select(f => f.ToString()).ToList()
+        );
     }
 }
