b3716e35b1f428b573633f5207bf71d07080533a
Andrei Timar <atimar999@gmail.com>
2025-03-29 10:59:25 +0200

    feat: implement get analysis console result

--- src/Application/Domain/Service.Project/Analysis/GetAnalysisResultsService.cs
+++ src/Application/Domain/Service.Project/Analysis/GetAnalysisResultsService.cs
@@ -1,22 +1,33 @@
-﻿using OneOf;
+﻿using DxWorks.ScriptBee.Plugin.Api.Services;
+using OneOf;
 using ScriptBee.Domain.Model.Analysis;
 using ScriptBee.Domain.Model.Project;
+using ScriptBee.Ports.Analysis;
+using ScriptBee.Ports.Files;
 using ScriptBee.UseCases.Project.Analysis;
 
 namespace ScriptBee.Service.Project.Analysis;
 
-public class GetAnalysisResultsService : IGetAnalysisResultsUseCase
+using GetConsoleResultType = OneOf<string, AnalysisDoesNotExistsError>;
+
+public class GetAnalysisResultsService(IGetAnalysis getAnalysis, IFileModelService fileModelService)
+    : IGetAnalysisResultsUseCase
 {
-    public Task<OneOf<string, AnalysisDoesNotExistsError>> GetConsoleResult(
+    public async Task<GetConsoleResultType> GetConsoleResult(
         ProjectId projectId,
         AnalysisId analysisId,
         CancellationToken cancellationToken = default
     )
     {
-        throw new NotImplementedException();
+        var result = await getAnalysis.GetById(analysisId, cancellationToken);
+
+        return await result.Match<Task<GetConsoleResultType>>(
+            async analysisInfo => await GetConsoleContent(analysisInfo, cancellationToken),
+            error => Task.FromResult<GetConsoleResultType>(error)
+        );
     }
 
-    public Task<
+    public async Task<
         OneOf<IEnumerable<AnalysisErrorResult>, AnalysisDoesNotExistsError>
     > GetErrorResults(
         ProjectId projectId,
@@ -24,15 +35,44 @@ public class GetAnalysisResultsService : IGetAnalysisResultsUseCase
         CancellationToken cancellationToken = default
     )
     {
-        throw new NotImplementedException();
+        var result = await getAnalysis.GetById(analysisId, cancellationToken);
+
+        return result.Match(analysisInfo => throw new NotImplementedException(), error => error);
     }
 
-    public Task<OneOf<IEnumerable<AnalysisFileResult>, AnalysisDoesNotExistsError>> GetFileResults(
+    public async Task<
+        OneOf<IEnumerable<AnalysisFileResult>, AnalysisDoesNotExistsError>
+    > GetFileResults(
         ProjectId projectId,
         AnalysisId analysisId,
         CancellationToken cancellationToken = default
     )
     {
-        throw new NotImplementedException();
+        var result = await getAnalysis.GetById(analysisId, cancellationToken);
+
+        return result.Match(analysisInfo => throw new NotImplementedException(), error => error);
+    }
+
+    private async Task<GetConsoleResultType> GetConsoleContent(
+        AnalysisInfo analysisInfo,
+        CancellationToken cancellationToken
+    )
+    {
+        var consoleResult = analysisInfo.Results.FirstOrDefault(r =>
+            r.Type == RunResultDefaultTypes.ConsoleType
+        );
+
+        if (consoleResult == null)
+        {
+            return "";
+        }
+
+        var fileStream = await fileModelService.GetFileAsync(
+            consoleResult.Id.ToFileId(),
+            cancellationToken
+        );
+        using var reader = new StreamReader(fileStream);
+
+        return await reader.ReadToEndAsync(cancellationToken);
     }
 }
---
+++ test/Application/Domain/Service.Project.Tests/Analysis/GetAnalysisResultsServiceTest.cs
@@ -0,0 +1,152 @@
+using DxWorks.ScriptBee.Plugin.Api.Services;
+using NSubstitute;
+using OneOf;
+using ScriptBee.Domain.Model.Analysis;
+using ScriptBee.Domain.Model.File;
+using ScriptBee.Domain.Model.Project;
+using ScriptBee.Domain.Model.ProjectStructure;
+using ScriptBee.Ports.Analysis;
+using ScriptBee.Ports.Files;
+using ScriptBee.Service.Project.Analysis;
+
+namespace ScriptBee.Service.Project.Tests.Analysis;
+
+public class GetAnalysisResultsServiceTest
+{
+    private readonly IGetAnalysis _getAnalysis = Substitute.For<IGetAnalysis>();
+    private readonly IFileModelService _fileModelService = Substitute.For<IFileModelService>();
+
+    private readonly GetAnalysisResultsService _getAnalysisResultsService;
+
+    public GetAnalysisResultsServiceTest()
+    {
+        _getAnalysisResultsService = new GetAnalysisResultsService(_getAnalysis, _fileModelService);
+    }
+
+    [Fact]
+    public async Task GivenNoAnalysis_WhenGetConsoleResult_ThenReturnAnalysisDoesNotExistsError()
+    {
+        var projectId = ProjectId.FromValue("id");
+        var analysisId = new AnalysisId(Guid.NewGuid());
+        _getAnalysis
+            .GetById(analysisId, Arg.Any<CancellationToken>())
+            .Returns(
+                Task.FromResult<OneOf<AnalysisInfo, AnalysisDoesNotExistsError>>(
+                    new AnalysisDoesNotExistsError(analysisId)
+                )
+            );
+
+        var result = await _getAnalysisResultsService.GetConsoleResult(projectId, analysisId);
+
+        result.AsT1.ShouldBe(new AnalysisDoesNotExistsError(analysisId));
+    }
+
+    [Fact]
+    public async Task GivenAnalysis_WhenGetConsoleResult_ThenReturnConsoleContent()
+    {
+        var projectId = ProjectId.FromValue("id");
+        var analysisId = new AnalysisId(Guid.NewGuid());
+        var resultId = new ResultId(Guid.NewGuid());
+        var analysisInfo = CreateAnalysisInfo(
+            analysisId,
+            projectId,
+            [
+                new ResultSummary(
+                    resultId,
+                    "ConsoleOutput",
+                    RunResultDefaultTypes.ConsoleType,
+                    DateTimeOffset.UtcNow
+                ),
+            ],
+            []
+        );
+        _getAnalysis
+            .GetById(analysisId, Arg.Any<CancellationToken>())
+            .Returns(
+                Task.FromResult<OneOf<AnalysisInfo, AnalysisDoesNotExistsError>>(analysisInfo)
+            );
+        _fileModelService
+            .GetFileAsync(resultId.ToFileId(), Arg.Any<CancellationToken>())
+            .Returns(Task.FromResult<Stream>(new MemoryStream("console content"u8.ToArray())));
+
+        var result = await _getAnalysisResultsService.GetConsoleResult(projectId, analysisId);
+
+        result.AsT0.ShouldBe("console content");
+    }
+
+    [Fact]
+    public async Task GivenAnalysisWithoutConsoleResult_WhenGetConsoleResult_ThenReturnEmptyConsoleContent()
+    {
+        var projectId = ProjectId.FromValue("id");
+        var analysisId = new AnalysisId(Guid.NewGuid());
+        var analysisInfo = CreateAnalysisInfo(analysisId, projectId, [], []);
+        _getAnalysis
+            .GetById(analysisId, Arg.Any<CancellationToken>())
+            .Returns(
+                Task.FromResult<OneOf<AnalysisInfo, AnalysisDoesNotExistsError>>(analysisInfo)
+            );
+
+        var result = await _getAnalysisResultsService.GetConsoleResult(projectId, analysisId);
+
+        result.AsT0.ShouldBe("");
+        await _fileModelService
+            .Received(0)
+            .GetFileAsync(Arg.Any<FileId>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task GivenNoAnalysis_WhenGetErrorResults_ThenReturnAnalysisDoesNotExistsError()
+    {
+        var projectId = ProjectId.FromValue("id");
+        var analysisId = new AnalysisId(Guid.NewGuid());
+        _getAnalysis
+            .GetById(analysisId, Arg.Any<CancellationToken>())
+            .Returns(
+                Task.FromResult<OneOf<AnalysisInfo, AnalysisDoesNotExistsError>>(
+                    new AnalysisDoesNotExistsError(analysisId)
+                )
+            );
+
+        var result = await _getAnalysisResultsService.GetErrorResults(projectId, analysisId);
+
+        result.AsT1.ShouldBe(new AnalysisDoesNotExistsError(analysisId));
+    }
+
+    [Fact]
+    public async Task GivenNoAnalysis_WhenGetFileResults_ThenReturnAnalysisDoesNotExistsError()
+    {
+        var projectId = ProjectId.FromValue("id");
+        var analysisId = new AnalysisId(Guid.NewGuid());
+        _getAnalysis
+            .GetById(analysisId, Arg.Any<CancellationToken>())
+            .Returns(
+                Task.FromResult<OneOf<AnalysisInfo, AnalysisDoesNotExistsError>>(
+                    new AnalysisDoesNotExistsError(analysisId)
+                )
+            );
+
+        var result = await _getAnalysisResultsService.GetFileResults(projectId, analysisId);
+
+        result.AsT1.ShouldBe(new AnalysisDoesNotExistsError(analysisId));
+    }
+
+    private static AnalysisInfo CreateAnalysisInfo(
+        AnalysisId analysisId,
+        ProjectId projectId,
+        IEnumerable<ResultSummary> results,
+        IEnumerable<AnalysisError> errors
+    )
+    {
+        return new AnalysisInfo(
+            analysisId,
+            projectId,
+            new ScriptId(Guid.NewGuid()),
+            new FileId(Guid.NewGuid()),
+            AnalysisStatus.Finished,
+            results,
+            errors,
+            DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow
+        );
+    }
+}
