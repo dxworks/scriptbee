62eb598f1df79bee49b53a96214f290aa83aa2e4
Andrei Timar <atimar999@gmail.com>
2025-04-04 21:10:32 +0300

    feat: implement GetInstanceContextService

--- src/Application/Domain/Service.Project/Context/GetInstanceContextService.cs
+++ src/Application/Domain/Service.Project/Context/GetInstanceContextService.cs
@@ -1,17 +1,31 @@
 using OneOf;
 using ScriptBee.Domain.Model.Context;
 using ScriptBee.Domain.Model.Errors;
+using ScriptBee.Ports.Instance;
 using ScriptBee.UseCases.Project.Context;
 
 namespace ScriptBee.Service.Project.Context;
 
-public class GetInstanceContextService : IGetInstanceContextUseCase
+using GetInstanceContextResult = OneOf<IEnumerable<ContextSlice>, InstanceDoesNotExistsError>;
+
+public class GetInstanceContextService(
+    IGetProjectInstance getProjectInstance,
+    IGetInstanceContext getInstanceContext
+) : IGetInstanceContextUseCase
 {
-    public Task<OneOf<IEnumerable<ContextSlice>, InstanceDoesNotExistsError>> Get(
+    public async Task<GetInstanceContextResult> Get(
         GetInstanceContextQuery query,
         CancellationToken cancellationToken = default
     )
     {
-        throw new NotImplementedException();
+        var result = await getProjectInstance.Get(query.InstanceId, cancellationToken);
+
+        return await result.Match<Task<GetInstanceContextResult>>(
+            async instanceInfo =>
+                GetInstanceContextResult.FromT0(
+                    await getInstanceContext.Get(instanceInfo, cancellationToken)
+                ),
+            error => Task.FromResult<GetInstanceContextResult>(error)
+        );
     }
 }
