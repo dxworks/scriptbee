6a4605217edf1ddb88a21f150c68f58e0347123b
Andrei Timar <atimar999@gmail.com>
2025-05-11 14:54:07 +0300

    refactor: removed the deprecated exception controller overload

--- src/Adapters/Driven/Marketplace.Client/Exceptions/PluginNotFoundException.cs
+++ src/Adapters/Driven/Marketplace.Client/Exceptions/PluginNotFoundException.cs
@@ -1,13 +1,4 @@
-using System.Runtime.Serialization;
-
 namespace ScriptBee.Marketplace.Client.Exceptions;
 
 [Serializable]
-public sealed class PluginNotFoundException : Exception
-{
-    public PluginNotFoundException(string message)
-        : base(message) { }
-
-    private PluginNotFoundException(SerializationInfo info, StreamingContext context)
-        : base(info, context) { }
-}
+public sealed class PluginNotFoundException(string message) : Exception(message);
--- src/Adapters/Driven/Marketplace.Client/Exceptions/PluginVersionNotFoundException.cs
+++ src/Adapters/Driven/Marketplace.Client/Exceptions/PluginVersionNotFoundException.cs
@@ -1,13 +1,4 @@
-using System.Runtime.Serialization;
-
 namespace ScriptBee.Marketplace.Client.Exceptions;
 
 [Serializable]
-public sealed class PluginVersionNotFoundException : Exception
-{
-    public PluginVersionNotFoundException(string message)
-        : base(message) { }
-
-    private PluginVersionNotFoundException(SerializationInfo info, StreamingContext context)
-        : base(info, context) { }
-}
+public sealed class PluginVersionNotFoundException(string message) : Exception(message);
