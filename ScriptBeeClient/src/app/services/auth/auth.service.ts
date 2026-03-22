d56d25c30fcf91dff50494cdf22eaac42bd13ee2
Andrei Timar <atimar999@gmail.com>
2026-03-22 16:50:35 +0200

    feat: remove Keycloak and OAuth2 dependencies from UI and Docker

--- ScriptBeeClient/src/app/services/auth/auth.service.ts
+++ ScriptBeeClient/src/app/services/auth/auth.service.ts
@@ -1,32 +1,15 @@
-import { computed, effect, inject, Injectable } from '@angular/core';
-import { KEYCLOAK_EVENT_SIGNAL, KeycloakEvent, KeycloakEventType, ReadyArgs, typeEventArgs } from 'keycloak-angular';
-import Keycloak from 'keycloak-js';
+import { Injectable, signal } from '@angular/core';
 import { IAuthService } from './iauth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService implements IAuthService {
-  private keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL);
+  authenticated = signal(true);
 
-  authenticated = computed(() => this.isAuthenticated(this.keycloakSignal()));
-
-  constructor(private readonly keycloak: Keycloak) {
-    effect(() => {
-      if (!this.authenticated()) {
-        this.keycloak.login().then();
-      }
-    });
-  }
+  constructor() {}
 
   logout() {
-    this.keycloak.logout().then();
-  }
-
-  private isAuthenticated(keycloakEvent: KeycloakEvent) {
-    if (keycloakEvent.type === KeycloakEventType.Ready) {
-      return typeEventArgs<ReadyArgs>(keycloakEvent.args);
-    }
-    return false;
+    this.authenticated.set(false);
   }
 }
