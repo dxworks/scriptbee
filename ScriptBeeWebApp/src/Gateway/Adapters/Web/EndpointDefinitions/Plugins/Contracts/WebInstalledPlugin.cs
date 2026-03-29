21779742bcf4d44ac11d76f1129077143bd59599
Andrei Timar <atimar999@gmail.com>
2026-03-29 13:18:58 +0300

    feat(backend): add installed plugins endpoint

---
+++ ScriptBeeWebApp/src/Gateway/Adapters/Web/EndpointDefinitions/Plugins/Contracts/WebInstalledPlugin.cs
@@ -0,0 +1,14 @@
+using ScriptBee.Domain.Model.Project;
+
+namespace ScriptBee.Web.EndpointDefinitions.Plugins.Contracts;
+
+public sealed record WebInstalledPlugin(string Id, string Version)
+{
+    public static WebInstalledPlugin Map(PluginInstallationConfig pluginInstallationConfig)
+    {
+        return new WebInstalledPlugin(
+            pluginInstallationConfig.PluginId,
+            pluginInstallationConfig.Version
+        );
+    }
+}
