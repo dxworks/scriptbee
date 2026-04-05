fd58d991da50a7b4b850ae7cf7fed23bdfe5dc77
Andrei Timar <atimar999@gmail.com>
2026-04-05 18:53:22 +0300

    feat(backend): update csharp sample code to use the correct import

--- ScriptBeeWebApp/src/Gateway/Adapters/Web/BackgroundServices/InstallPluginsForAllocatedInstancesBackgroundService.cs
+++ ScriptBeeWebApp/src/Gateway/Adapters/Web/BackgroundServices/InstallPluginsForAllocatedInstancesBackgroundService.cs
@@ -6,19 +6,32 @@ namespace ScriptBee.Web.BackgroundServices;
 
 public class InstallPluginsForAllocatedInstancesBackgroundService(
     Channel<InstanceAllocatedEvent> eventChannel,
-    InstallPluginsForNewlyAllocatedInstance service
+    InstallPluginsForNewlyAllocatedInstance service,
+    ILogger<InstallPluginsForAllocatedInstancesBackgroundService> logger
 ) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (await eventChannel.Reader.WaitToReadAsync(stoppingToken))
         {
-            var request = await eventChannel.Reader.ReadAsync(stoppingToken);
-            await service.InstallPlugins(
-                request.projectDetails,
-                request.instanceInfo,
-                stoppingToken
-            );
+            InstanceAllocatedEvent? request = null;
+            try
+            {
+                request = await eventChannel.Reader.ReadAsync(stoppingToken);
+                await service.InstallPlugins(
+                    request.projectDetails,
+                    request.instanceInfo,
+                    stoppingToken
+                );
+            }
+            catch (Exception e)
+            {
+                logger.LogError(
+                    e,
+                    "An error occurred while trying to install plugins for {Request}",
+                    request
+                );
+            }
         }
     }
 }
