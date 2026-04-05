85d5672cd09606e3e90256b5203c5e7f2a7ea124
Andrei Timar <33547890+andrei2699@users.noreply.github.com>
2026-04-05 15:11:18 +0300

    ci: add multiple workflow files (#124)

--- ScriptBeeClient/src/app/services/theme/theme.service.spec.ts
+++ ScriptBeeClient/src/app/services/theme/theme.service.spec.ts
@@ -1,12 +1,17 @@
+import { beforeEach, describe, expect, it } from 'vitest';
 import { TestBed } from '@angular/core/testing';
-
 import { ThemeService } from './theme.service';
 
 describe('ThemeServiceService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [ThemeService],
+    })
+  );
 
   it('should be created', () => {
     const service: ThemeService = TestBed.inject(ThemeService);
+
     expect(service).toBeTruthy();
   });
 });
