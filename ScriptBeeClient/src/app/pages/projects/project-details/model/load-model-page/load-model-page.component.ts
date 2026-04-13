445cbd471077e302ef1da37e6975b74a76ab64a6
Andrei Timar <atimar999@gmail.com>
2026-04-13 08:24:35 +0300

    feat(UI): move load models in separate page

---
+++ ScriptBeeClient/src/app/pages/projects/project-details/model/load-model-page/load-model-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { MatDivider } from '@angular/material/list';
+import { ProjectStateService } from '../../../../../services/projects/project-state.service';
+import { Project } from '../../../../../types/project';
+import { LoadModelsComponent } from './load-models/load-models.component';
+import { LinkModelsComponent } from './link-models/link-models.component';
+
+@Component({
+  selector: 'app-load-model-page',
+  imports: [MatDivider, LoadModelsComponent, LinkModelsComponent],
+  templateUrl: './load-model-page.component.html',
+  styleUrl: './load-model-page.component.scss',
+})
+export class LoadModelPage {
+  private projectStateService = inject(ProjectStateService);
+
+  project = input.required<Project>();
+  instanceId = computed(() => this.projectStateService.currentInstanceId());
+}
