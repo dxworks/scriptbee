34b5fede6c99b095eeda4a5a37744407d551049e
Andrei Timar <atimar999@gmail.com>
2026-04-14 14:41:04 +0300

    ci: update extract_changelog.sh to strip brackets

--- scripts/extract_changelog.sh
+++ scripts/extract_changelog.sh
@@ -11,7 +11,19 @@ OUTPUT_FILE=${3:-RELEASE_NOTES.md}
 
 echo "Extracting changelog for version $VERSION from $CHANGELOG_PATH"
 
-awk "/^## \[$VERSION\]/,/^## \[/" "$CHANGELOG_PATH" | sed '$d' > "$OUTPUT_FILE"
+awk -v ver="$VERSION" '
+    BEGIN {
+        # Strip brackets if provided in the version argument
+        gsub(/^\[+|\]+$/, "", ver)
+        # Escape dots for regex safety
+        gsub(/\./, "\\.", ver)
+        start_pattern = "^## \\[" ver "\\]"
+        end_pattern = "^## \\["
+    }
+    $0 ~ start_pattern { p=1; next }
+    $0 ~ end_pattern && p { p=0; exit }
+    p { print }
+' "$CHANGELOG_PATH" > "$OUTPUT_FILE"
 
 if [ ! -s "$OUTPUT_FILE" ]; then
     echo " No changelog entry found for version $VERSION"
