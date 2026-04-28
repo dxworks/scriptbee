b69a47596730413e6aa3e04b7f481187c00986e3
Andrei Timar <atimar999@gmail.com>
2026-04-28 08:01:36 +0300

    refactor(backend): removed AbstractNodeNodeTypeResolver workaround and use the builting feature for polymorphic types

--- ScriptBeeWebApp/src/Plugins/Plugins/Yaml/ITypeDiscriminator.cs
+++
@@ -1,8 +0,0 @@
-namespace ScriptBee.Plugins.Yaml;
-
-public interface ITypeDiscriminator
-{
-    Type BaseType { get; }
-
-    bool TryResolve(ParsingEventBuffer buffer, out Type? suggestedType);
-}
