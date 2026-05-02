ac1d7403b4e9efec84f5d2230bcf5e19635680a7
Andrei Timar <33547890+andrei2699@users.noreply.github.com>
2026-05-02 22:21:32 +0300

    feat: update UI to support native federation (#239)
    * feat(backend): add endpoint for gateway plugins ui manifest
    
    * feat(UI): add native federation

--- ScriptBeeClient/src/main.ts
+++ ScriptBeeClient/src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { initFederation } from '@angular-architects/native-federation';
 
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+initFederation('/api/plugins/gateway/ui/manifest')
+  .catch((err) => console.error(err))
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  .then((_) => import('./bootstrap'))
+  .catch((err) => console.error(err));
