0b323d0aca9aab1f9dd3d2cff3d10777386c1d46
Andrei Timar <33547890+andrei2699@users.noreply.github.com>
2026-07-08 22:46:55 +0300

    feat(charts): add bar chart component (#274)

--- Plugins/UI/default-scriptbee-charts/src/app/app.spec.ts
+++ Plugins/UI/default-scriptbee-charts/src/app/app.spec.ts
@@ -1,10 +1,20 @@
+import { computed, signal } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { ThemeService } from './services/theme.service';
 
-describe('App', () => {
+describe('AppComponent with Mock Theme', () => {
+  let mockThemeService: Partial<ThemeService>;
+
   beforeEach(async () => {
+    mockThemeService = {
+      isDark: signal(false),
+      echartsTheme: computed(() => 'light'),
+    };
+
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [{ provide: ThemeService, useValue: mockThemeService }],
     }).compileComponents();
   });
 
---
+++ Plugins/UI/default-scriptbee-charts/src/app/components/bar-chart/bar-chart.ts
@@ -0,0 +1,34 @@
+import { Component, computed, input } from '@angular/core';
+import { EChartsCoreOption } from 'echarts';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import { BarChart as EchartsBarChart } from 'echarts/charts';
+import { GridComponent, LegendComponent, TooltipComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+import { BarChartInput, ChartParameters } from '../../types/ChartInput';
+
+echarts.use([EchartsBarChart, GridComponent, CanvasRenderer, LegendComponent, TooltipComponent]);
+
+@Component({
+  selector: 'app-bar-chart',
+  imports: [NgxEchartsDirective],
+  templateUrl: './bar-chart.html',
+  styleUrl: './bar-chart.scss',
+  providers: [provideEchartsCore({ echarts })],
+})
+export class BarChart {
+  parameters = input.required<ChartParameters<BarChartInput>>();
+
+  options = computed<EChartsCoreOption>(() => {
+    const input = this.parameters().input;
+
+    return {
+      ...(input.options ?? {}),
+      legend: input.legend,
+      tooltip: input.tooltip,
+      xAxis: input.xAxis,
+      yAxis: input.yAxis,
+      series: (input.series ?? []).map((s) => ({ ...s, type: 'bar' })),
+    };
+  });
+}
