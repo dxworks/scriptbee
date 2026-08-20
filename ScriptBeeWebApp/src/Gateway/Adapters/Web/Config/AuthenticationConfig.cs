09cfeb41aeefb91d370b9fa98912064f443bc19c
Andrei Timar <atimar999@gmail.com>
2026-08-20 09:05:50 +0300

    feat: add more configurations for the auth config and expose AuthWellknownEndpointUrl

--- ScriptBeeWebApp/src/Gateway/Adapters/Web/Config/AuthenticationConfig.cs
+++ ScriptBeeWebApp/src/Gateway/Adapters/Web/Config/AuthenticationConfig.cs
@@ -3,7 +3,14 @@ namespace ScriptBee.Web.Config;
 public class AuthenticationConfig
 {
     public string? AuthMode { get; init; }
+    public required bool RequireHttpsMetadata { get; init; }
     public string? Authority { get; init; }
+    public string? Audience { get; init; }
+    public string? AuthWellknownEndpointUrl { get; init; }
     public string? ClientId { get; init; }
     public string? Scope { get; init; }
+    public string? OpaUrl { get; init; }
+
+    public bool IsDevelopment =>
+        AuthMode?.Equals("Development", StringComparison.OrdinalIgnoreCase) ?? false;
 }
